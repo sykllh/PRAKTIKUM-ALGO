de3c854ddd9ea3616f6a299ebc07c4c7a5a8e229
sykllh <sykllhidmi@gmail.com>
2026-03-06 15:37:06 +0700

    menambahkan void Bubble sort

--- bubblee_sort.cpp
+++ bubblee_sort.cpp
@@ -8,6 +8,53 @@ int n;
 void input()
 {
         while (true)
-}
     cout << "Masukkan panjang array : ";
     cin >> n;
+
+        if (n <=20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "ERROR : Maksimal input array adalah 20/n";
+        }
+
+        cout << endl;
+        cout << "=====================" << endl;
+        cout << "Masukkan elemen Array" << endl;
+        cout << "=====================" << endl;
+
+        for (int i = 0; i < n; i++) //i++ /i = i+1
+        {
+            cout << "Masukkan nilai ke - " << i + 1 << " : ";
+            cin >> a[i];
+        }
+    }
+
+    void display()
+    {
+        cout << endl;
+        cout << "==========================" << endl;
+        cout << "Elemen Array telah terurut" << endl;
+        cout << "==========================" << endl;
+
+        forr (int i = 0; i < n; i++)
+        {
+            cout << "Data ke - " << i << " : " << a[i] << endl;
+        }
+    }
+
+    void BubbleSort()
+    {
+        cout << "\n================" <<endl;
+        cout << "Hasil Setiap Pass" << endl;
+        cout << "==================" << endl;
+        for (int pass = 1; pass < n; pass++)
+        {
+            for (int j = 0; j <= n -1 - pass; j++)
+            {
+                if
+            }
+        }
+    }
