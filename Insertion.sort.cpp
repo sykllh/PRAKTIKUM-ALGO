f23b3ad0177a2dedf38cd877a51788a00d2ec982
sykllh <sykllhidmi@gmail.com>
2026-03-13 14:21:01 +0700

    menambahkan prosedur insertion sort

--- Insertion.sort.cpp
+++ Insertion.sort.cpp
@@ -6,6 +6,27 @@ int n;            //membuat variable inputan n
 
 void input (){ //procedure input
     while (true){
-        cout << "Masukan Jumlah Data Pada Array"
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
     }
+    cout << endl;
+    cout << "==============================================";
+    cout << "=========Masukkan Element Array===============";
+    cout << "==============================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionsort(){  //procedure insertion sort
+
 }
