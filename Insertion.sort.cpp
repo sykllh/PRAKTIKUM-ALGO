5e51cd70a25ab8fef1b8e51c033a55a9362188a6
sykllh <sykllhidmi@gmail.com>
2026-03-13 14:45:50 +0700

    menambahkan variabel inputan

--- Insertion.sort.cpp
+++ Insertion.sort.cpp
@@ -28,5 +28,44 @@ void input (){ //procedure input
 }
 
 void insertionsort(){  //procedure insertion sort
+    int temp;
+    int j;
 
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i <<": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+
+}
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] <<endl;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    insertionsort();
+    display();
+    system("pause");
 }
